1ef1d55a7f6615ced0353c7a99bbf25240a1e796
unknown <menhior1@gmail.com>
2020-08-08 15:24:06 +0400

    make it better

--- mainkaya/settings.py
+++ mainkaya/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['zz-rps.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['zz-ecom.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
