cc74c9f55c76ed1fa96fce555afdb1010b66135a
unknown <menhior1@gmail.com>
2020-08-08 21:28:57 +0400

    make it better

--- shop/models.py
+++ shop/models.py
@@ -45,7 +45,7 @@ class Product(models.Model):
     digital = models.BooleanField(default=False, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     description = models.CharField(max_length=500, null=True, blank=True)
-    tags = models.ManyToManyField(Tag, null=True, blank=True)
+    """tags = models.ManyToManyField(Tag, null=True, blank=True)"""
 
     def __str__(self):
         return self.name
