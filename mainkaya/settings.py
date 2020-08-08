c41b87e6275170e99114e06a84b4f3d2b33b1383
unknown <menhior1@gmail.com>
2020-08-09 03:29:42 +0400

    cloudinary

--- mainkaya/settings.py
+++ mainkaya/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
